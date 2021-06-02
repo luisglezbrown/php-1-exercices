3bec5bc5fffd3abc1aea2ac81110bf309385388d
luisglezbrown <lfernanglez@gmail.com>
2021-06-02 14:03:07 +0200

    Comenzamos a servir info

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hola Luis</h1>
+    <p>Hoy es <strong><?php print_r(getdate()['wday']) ?></strong>. ¿Qué tal estás?</strong></p>
+</body>
+</html>
+
+
