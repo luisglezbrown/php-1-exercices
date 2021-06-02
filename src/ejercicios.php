5f20620646ddff97cd16849adc59fcc1158fcec3
luisglezbrown <lfernanglez@gmail.com>
2021-06-02 11:09:40 +0200

    Corregimos ejercicio SumArray

--- src/ejercicios.php
+++ src/ejercicios.php
@@ -1,1 +1,22 @@
 <?php
+
+declare(strict_types=1);
+
+// EjercicioSumArrayTest
+function sumArray(array $array): int {
+    $sum = 0;
+    for($index = 0; $index < sizeof($array); $index++){
+        $sum += $array[$index];
+    }
+
+    return $sum;
+}
+
+function sumArray2(array $array): int {
+    $sum = 0;
+    foreach ($array as $element) {
+        $sum += $element;
+    }
+
+    return $sum;
+}
--- tests/EjercicioSumArrayTest.php
+++ tests/EjercicioSumArrayTest.php
@@ -7,10 +7,12 @@ require_once('./src/ejercicios.php');
 
 final class EjercicioSumArrayTest extends TestCase
 {
+
   // Programar un método que sume todos los elementos de un array y devuelva el resultado
   public function testSumArray(): void
   {
     $input = [3, 6, 1, -5, 0, 4];
     assertEquals(9, sumArray($input));
+    assertEquals(9, sumArray2($input));
   }
 }
