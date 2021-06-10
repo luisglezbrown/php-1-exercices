394290c7b61ba3d25504eaebfd6453a168664e30
luisglezbrown <lfernanglez@gmail.com>
2021-06-10 11:25:23 +0200

    Añadir logica para eliminar un registro

--- public/employees_delete.php
+++ public/employees_delete.php
@@ -1,9 +1,23 @@
 <?php
+require $_SERVER['DOCUMENT_ROOT'] . '/lib/app.php';
 
 $body = file_get_contents('php://input');
 
 $person = json_decode($body, true);
 
-echo json_encode($person);
+$query = 'DELETE FROM employees WHERE id = :identificador';
+
+$stm = $dbConnection->prepare($query);
+$stm->bindParam(':identificador', $person['id']);
+$stm->execute();
+
+$result = $stm->rowCount();
+
+$response = [
+    'status' => $result === 0 ? 'error' : 'success',
+    'message' => $result === 0 ? 'No se eliminaron registros' : 'He eliminado '.$result.' registros'
+];
+
+echo json_encode($response); 
 
 exit();
